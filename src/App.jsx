302e21176799fa264e21db1adeaecdbd540c0293
saurabh sharma <aswap42@gmail.com>
2026-04-02 00:29:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import StringGenerator from './pages/StringGenerator'
 import Translator from './pages/Translator'
 
 function App() {
   return (
-    <BrowserRouter basename="/qskill-internship">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/generator" element={<StringGenerator />} />
         <Route path="/translator" element={<Translator />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
